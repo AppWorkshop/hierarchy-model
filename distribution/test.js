60d96236d9fc38463cc474c70388d76f5de44ab9
Mike Cunneen <mike@appworkshop.net>
2017-09-18 14:01:45 +0800

    fix invalid reference to hierarchy.min.js in package.json

--- distribution/test.js
+++ distribution/test.js
@@ -1856,7 +1856,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
         var _treeModel2 = _interopRequireDefault(_treeModel);
 
-        var _topiary = __webpack_require__(17);
+        var _topiary = __webpack_require__(6);
 
         var _topiary2 = _interopRequireDefault(_topiary);
 
@@ -2520,8 +2520,55 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       }();
 
       /***/
-    },,
+    },
     /* 6 */
+    /***/function (module, exports) {
+
+      module.exports = function (tree, recurseName, opts) {
+        opts = opts || {};
+        var nameFn = opts.name || function (e) {
+          return e.name;
+        };
+        var filterFn = opts.filter || function () {
+          return true;
+        };
+        var sortFn = !opts.sort ? null : function (x, y) {
+          return nameFn(x).localeCompare(nameFn(y));
+        };
+
+        var lines = [nameFn(tree)];
+        if (!Array.isArray(tree[recurseName])) {
+          throw new Error("No recurse entry for '" + recurseName + "' found on root");
+        }
+
+        var recurse = function recurse(xs, level, parentAry) {
+          var ys = sortFn instanceof Function ? xs.sort(sortFn) : xs;
+          ys.forEach(function (sub, idx, subAry) {
+            var children = (sub[recurseName] || []).filter(filterFn),
+                hasChildren = children.length > 0,
+                forkChar = hasChildren ? "┬" : "─",
+                isLast = idx === subAry.length - 1,
+                turnChar = isLast ? "└" : "├",
+                indent = '';
+
+            for (var i = 0; i < level; i += 1) {
+              indent += (parentAry[i] ? " " : "│") + " ";
+            }
+
+            lines.push(" " + indent + turnChar + "─" + forkChar + nameFn(sub));
+
+            if (hasChildren) {
+              // recurse into current tree while keeping track of parent lines
+              recurse(children, level + 1, parentAry.concat(isLast));
+            }
+          });
+        };
+        recurse(tree[recurseName].filter(filterFn), 0, []);
+        return lines.join('\n');
+      };
+
+      /***/
+    },
     /* 7 */
     /***/function (module, exports, __webpack_require__) {
 
@@ -4093,63 +4140,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
           }.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
         }
       }).call(this);
-
-      /***/
-    },,,,,,,,,,
-    /* 8 */
-    /* 9 */
-    /* 10 */
-    /* 11 */
-    /* 12 */
-    /* 13 */
-    /* 14 */
-    /* 15 */
-    /* 16 */
-    /* 17 */
-    /***/function (module, exports) {
-
-      module.exports = function (tree, recurseName, opts) {
-        opts = opts || {};
-        var nameFn = opts.name || function (e) {
-          return e.name;
-        };
-        var filterFn = opts.filter || function () {
-          return true;
-        };
-        var sortFn = !opts.sort ? null : function (x, y) {
-          return nameFn(x).localeCompare(nameFn(y));
-        };
-
-        var lines = [nameFn(tree)];
-        if (!Array.isArray(tree[recurseName])) {
-          throw new Error("No recurse entry for '" + recurseName + "' found on root");
-        }
-
-        var recurse = function recurse(xs, level, parentAry) {
-          var ys = sortFn instanceof Function ? xs.sort(sortFn) : xs;
-          ys.forEach(function (sub, idx, subAry) {
-            var children = (sub[recurseName] || []).filter(filterFn),
-                hasChildren = children.length > 0,
-                forkChar = hasChildren ? "┬" : "─",
-                isLast = idx === subAry.length - 1,
-                turnChar = isLast ? "└" : "├",
-                indent = '';
-
-            for (var i = 0; i < level; i += 1) {
-              indent += (parentAry[i] ? " " : "│") + " ";
-            }
-
-            lines.push(" " + indent + turnChar + "─" + forkChar + nameFn(sub));
-
-            if (hasChildren) {
-              // recurse into current tree while keeping track of parent lines
-              recurse(children, level + 1, parentAry.concat(isLast));
-            }
-          });
-        };
-        recurse(tree[recurseName].filter(filterFn), 0, []);
-        return lines.join('\n');
-      };
 
       /***/
     }]
